a7042952aae3c20860ccfaab47a1d1fd6e7f3d5f
Jesus <jgarcia28599@gmail.com>
2022-09-15 23:51:31 -0300

    try

--- database/config.js
+++ database/config.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async () => {
+const dbConnection =  () => {
     try {
 
-        await mongoose.connect("mongodb+srv://admin_node:ZdS4azpu9hdvhCDc@hnktech.1lmhr.mongodb.net/hnkdb", { useNewUrlParser: true });
+        const uri = process.env.MONGODB_CNN;
+
+        mongoose.connect(uri , { useNewUrlParser: true });
 
     } catch (error) {
         console.error(error);
